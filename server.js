12301b11748908b6723e5e676a79c669872c1d23
geanemr <geaneramos1995@gmail.com>
2024-11-19 17:12:56 -0300

    chore: create json objects with data

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 import express from "express";
 
+const posts = [
+  {
+    id: 1,
+    descricao: "foto de gato",
+    imagem: `https://placecats.com/millie/300/150`,
+  },
+  {
+    id: 2,
+    descricao: "gato brincando",
+    imagem: `https://placecats.com/millie/300/151`,
+  },
+  {
+    id: 3,
+    descricao: "gato correndo",
+    imagem: `https://placecats.com/millie/300/152`,
+  },
+];
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("servidor escutando");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("boas vindas à imersão!"); //http://localhost:3000/api
+app.get("/posts", (req, res) => { //http://localhost:3000/posts
+  res.status(200).json(posts);
+});
+
+function getPostsById(id) {
+  return posts.findIndex((post) => {
+    return post.id === Number(id);
+  });
+}
+
+app.get("/posts/:id", (req, res) => {
+  const index = getPostsById(req.params.id);
+  res.status(200).json(posts[index]); //http://localhost:3000/posts/1
 });
