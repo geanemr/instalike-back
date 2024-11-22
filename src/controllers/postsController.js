fca5df3d6aa9b4bf0262533c8b699fe02f0b4f5f
geanemr <geaneramos1995@gmail.com>
2024-11-21 21:30:05 -0300

    feat: create new post and upload new image

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,6 +1,31 @@
-import getAllPosts from "../models/postsModel.js";
+import fs from "fs";
+import { getAllPosts, createNewPost } from "../models/postsModel.js";
 
 export async function listAllPosts(req, res) {
   const posts = await getAllPosts();
   res.status(200).json(posts);
+}
+
+export async function newPost(req, res) {
+  const newPost = req.body;
+  try {
+    const createdPost = await createNewPost(newPost);
+    res.status(200).json(createdPost);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ Error: "Request failed." });
+  }
+}
+
+export async function uploadImage(req, res) {
+  const newPost = req.body;
+  try {
+    const createdPost = await createNewPost(newPost);
+    const updatedImage = `uploads/${createdPost.insertedId}.png`;
+    fs.renameSync(req.file.path, updatedImage);
+    res.status(200).json(createdPost);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ Error: "Request failed." });
+  }
 }
--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -2,8 +2,14 @@ import connectToDatabase from "../config/dbConfig.js";
 
 const connection = await connectToDatabase(process.env.STRING_CONEXAO);
 
-export default async function getAllPosts() {
+export async function getAllPosts() {
   const db = connection.db("imersao-instalike");
   const collection = db.collection("posts");
   return collection.find().toArray();
+}
+
+export async function createNewPost(newPost) {
+  const db = connection.db("imersao-instalike");
+  const collection = db.collection("posts");
+  return collection.insertOne(newPost); //insertOne é do Mongodb
 }
